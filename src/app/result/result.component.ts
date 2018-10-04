c6740c7ed902e36c71be8c77daeeb344ac50738a
Andrew Baumann <andrew@mrbaumann.me>
2018-10-04 11:10:05 -0700

    Map Touchups

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -1,43 +1,46 @@
-import { Component, OnInit, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Solution, SolutionEntry } from '../domain';
-
+import { Subject, Observable, of, merge } from 'rxjs';
 import { ApiService } from '../service/api.service';
 
+
 @Component({
-//  selector: 'my-app',
+  //  selector: 'my-app',
   templateUrl: './result.component.html',
   styleUrls: ['./result.component.css']
 })
 export class ResultComponent implements OnInit {
+  lat: Number = 34.7714347;
+  lng: Number = -98.317331;
+  public waypoints: {
+    location: { lat: number; lng: number };
+    stopover: boolean;
+  }[];
 
-  lat: Number = 41.85;
-  lng: Number = -87.65;
-  public waypoints: {location: { lat: number, lng: number }, stopover: boolean}[];
+  constructor(private api: ApiService) {}
 
-  constructor(private api: ApiService) {
-  }
-
-  origin: String = 'Saint Lous, MO' ;
+  public flImagePath = './assets/hurricane-60x60.png';
+  public bluePin = './assets/blue-dot-40x40.png';
+  public truckLocation = './assets/truck-40x40.png';
+  origin: String = 'Saint Lous, MO';
   public markerOptions = {
     origin: {
-        icon: './assets/truck-40x40.png',
-        draggable: false,
+      icon: this.truckLocation,
+      draggable: false
     }
-};
-
- public flImagePath = './assets/hurricane-60x60.png';
+  };
 
   public renderOptions = {
     suppressMarkers: true
   };
 
-  destination: { lat: number, lng: number };
+  destination: { lat: number; lng: number };
   public solution: Solution;
   public allAccountsMarkers: SolutionEntry[];
   public markers: SolutionEntry[];
 
   public loading = true;
-
+  public calculatedResults = false;
   public screen = Screen.ALL;
 
   ngOnInit() {
@@ -62,7 +65,13 @@ export class ResultComponent implements OnInit {
         this.loading = false;
       }
       console.log(this.markers);
+      setTimeout(() => this.resultsAreReady(), 31000);
     });
+  }
+
+  resultsAreReady() {
+    console.log('timer hit!');
+    this.calculatedResults = true;
   }
 
   setShowRoute() {
@@ -80,18 +89,19 @@ export class ResultComponent implements OnInit {
 
   setSolution(s: Solution) {
     this.solution = s;
-        const temp = s.getWaypoints();
-        console.log('waypoints: ' + temp);
-        console.log(temp);
-        this.markers = s.getLatLongArray();
-        console.log('markers: ' + this.markers);
-        console.log(this.markers);
-        const cut: {location: { lat: number, lng: number }, stopover: boolean}[] = temp.slice(0, temp.length - 2);
-        this.waypoints = cut;
-        this.destination = temp[ temp.length - 1].location;
+    const temp = s.getWaypoints();
+    console.log('waypoints: ' + temp);
+    console.log(temp);
+    this.markers = s.getLatLongArray();
+    console.log('markers: ' + this.markers);
+    console.log(this.markers);
+    const cut: {
+      location: { lat: number; lng: number };
+      stopover: boolean;
+    }[] = temp.slice(0, temp.length - 2);
+    this.waypoints = cut;
+    this.destination = temp[temp.length - 1].location;
   }
-
-
 
   // blackLabel(index: number): {color: string, fontWeight: string, text: string} {
   //   console.log('This might hurt.');
